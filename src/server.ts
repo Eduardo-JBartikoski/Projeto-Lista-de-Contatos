f3dd8d53b08362f0d8ecd1e33885967110433d50
Eduardo-Bartikoski <eduardob.i207@gmail.com>
2026-02-23 18:07:01 -0300

    servidor funcional e rotas

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,20 @@
-import express from 'express';
+import express, { Router } from 'express';
 import helmet from 'helmet';
+import router from './routes/index.js';
 
 
 const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(express.urlencoded( { extended: true } ));
+
+server.use('/', router);
+
+server.listen(3000, () => {
+    server.response
+       console.log("Servidor funcionando, http://localhost:3000/");
+})
+
+
+
